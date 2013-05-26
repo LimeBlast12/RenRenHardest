f801831eeed90d1bc7b180d50755b2042e60599d
Danni Wang <danniwang12@gmail.com>
2013-05-26 09:51:03 +0800

    改进计时器。

--- src/view/GameMainActivity.java
+++ src/view/GameMainActivity.java
@@ -23,36 +23,45 @@ import android.widget.TextView;
 
 public class GameMainActivity extends Activity {
 	
+	private ImageView mImageView = null;
 	private TextView mTextView_number = null;
 	private TextView mTextView_time = null;
 	private Button mButton_filter_grey = null;
 	private Button mButton_filter_old = null;
 	private Button mButton_friend = null;
-	/*用于计时*/
-	private static int total_time = 10;//表示一轮游戏的总时间
-	private static int count = 0;//从开始到当前的秒数
-	private static int delay = 1000;  //1s
-	private static int period = 1000;  //1s
+	/*用于计时，有些变量只能是静态变量且不应修改，有些如count切忌设为静态变量*/
+	private static final int total_time = 10; //表示一轮游戏的总时间
+	private static final int delay = 1000;  //1s
+	private static final int period = 1000;  //1s
 	private static final int UPDATE_TEXTVIEW = 0;
+	private int count = 0; //从开始到当前的秒数
 	private Timer mTimer = null;
 	private TimerTask mTimerTask = null;
 	private Handler mHandler = null;
 	/*用于更换图片*/
-	private ArrayList<ImageDisplay> imageList;//每一个map存放url, 头像主人的标识（自己为0，好友为1）, 滤镜种类（黑白是0，复古是1）。
-	private int current_index = 0;
+	private ArrayList<ImageDisplay> imageList; //ImageDisplay是一个包含三项图片信息的对象
+	private int current_index = 1; //表示当前遍历到的图片index,因为刚进入游戏已经展示第0张图片，故设为1
+	ImageLoader imageLoader = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_mainview);
 		initButtons();
-		/*初始化imageList*/
+		/*initTextViews*/
+		mTextView_time = (TextView)findViewById(R.id.time_left);
+		mTextView_number = (TextView)findViewById(R.id.image_number);
+		/*初始化imageList，这里先写死*/
 		imageList = new ArrayList<ImageDisplay>();
 		imageList.add(new ImageDisplay("http://hdn.xnimg.cn/photos/hdn521/20130508/2110/h_large_QUg5_7e8f0000005b113e.jpg",0,0));
 		imageList.add(new ImageDisplay("http://hdn.xnimg.cn/photos/hdn221/20120917/2305/h_large_o5GZ_3a09000032131375.jpg",0,0));
 		imageList.add(new ImageDisplay("http://hdn.xnimg.cn/photos/hdn521/20120908/2030/h_large_nYKP_77f600000b4d1376.jpg",0,0));
 		imageList.add(new ImageDisplay("http://hdn.xnimg.cn/photos/hdn321/20120901/2005/h_large_n9cT_5cde000024bc1376.jpg",0,0));
 		imageList.add(new ImageDisplay("http://hdn.xnimg.cn/photos/hdn421/20120717/0830/h_large_4FNU_4d0a000004c61375.jpg",0,0));
+		/*初始化imageView,imageLoader*/
+		mImageView = (ImageView)findViewById(R.id.iv);
+		setImage(0);
+		imageLoader = new ImageLoader(GameMainActivity.this);
 		/*计时开始*/
 		timer();
 	}
@@ -67,54 +76,57 @@ public class GameMainActivity extends Activity {
 	private void initButtons() {
 		mButton_filter_grey = (Button) findViewById(R.id.filter_grey);
 		mButton_filter_grey.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				change_image();
+				change_image_and_number();
 			}
 		});
 		
 		mButton_filter_old = (Button) findViewById(R.id.filter_old);
 		mButton_filter_old.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				change_image();
+				change_image_and_number();
 			}
 		});
 		
 		mButton_friend = (Button) findViewById(R.id.friend);
 		mButton_friend.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				change_image();
+				change_image_and_number();
 			}
 		});
 	}
 	
-	/*change_image与更换图像有关*/
-	private void change_image(){
+	/*change_image与更换图片有关*/
+	private void change_image_and_number(){
 		/*这里的5是一组图片的总数*/
 		if(current_index<5){
-			ImageLoader imageLoader = new ImageLoader(GameMainActivity.this);
-			Bitmap originalBitmap = imageLoader.getBitmap(imageList.get(current_index).getUrl());
-			ImageView iv = (ImageView)findViewById(R.id.iv);
-	        iv.setImageBitmap(originalBitmap);
+			/*先改变图片*/
+			setImage(current_index);
+	        /*再改变数字*/
+	        int number = current_index+1;
+			mTextView_number.setText("No."+String.valueOf(number));
+			
 	        current_index++;
 		}
 	}
 	
+	public void setImage(int index){
+		Bitmap originalBitmap = imageLoader.getBitmap(imageList.get(index).getUrl());
+		mImageView = (ImageView)findViewById(R.id.iv);
+		mImageView.setImageBitmap(originalBitmap);
+	}
+	
 	/*timer, sendMessage, updateTextView这3个方法都与计时有关*/
 	private void timer(){
-		mTextView_time = (TextView)findViewById(R.id.time_left);
-		
 		mHandler = new Handler(){
 			@Override
 			public void handleMessage(Message msg) {
 				switch (msg.what) {
 				case UPDATE_TEXTVIEW:
-					updateTextView();
+					updateTextView_time();
 					break;
 				default:
 					break;
@@ -129,15 +141,13 @@ public class GameMainActivity extends Activity {
 			mTimerTask = new TimerTask() {
 				@Override
 				public void run() {
-					sendMessage(UPDATE_TEXTVIEW);
-					
+					sendMessage(UPDATE_TEXTVIEW);					
 					do {
 						try {
 							Thread.sleep(1000);
 						} catch (InterruptedException e) {
 						}	
 					} while (count-total_time>=0);
-					
 					count ++;  
 				}
 			};
@@ -154,8 +164,8 @@ public class GameMainActivity extends Activity {
 		}
 	}
 	
-	private void updateTextView(){
-		mTextView_time.setText(String.valueOf(total_time-count));
+	private void updateTextView_time(){
+		int time_left = total_time-count;
+		mTextView_time.setText(String.valueOf(time_left));
 	}
-
 }
