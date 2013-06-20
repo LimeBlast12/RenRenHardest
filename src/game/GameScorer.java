9c3df6f1854946d61cff1145f55fcb0a4596b788
lys101250099 <luoyishuai@gmail.com>
2013-06-20 16:32:08 +0800

    又修改了计分系统

--- src/game/GameScorer.java
+++ src/game/GameScorer.java
@@ -3,36 +3,41 @@ package game;
 
 public class GameScorer {
 	
-	private static long numOfMyImages;       				// 自己所有的头像
-	private static long numOfMyFriendsImages;				// 好友所有的头像	
-	private static long numOfRightImages;					// 猜对的头像张数
-	private static long numOfWrongImages;					// 猜错的头像张数
+	private static int numOfMyImages;       				// 自己所有的头像
+	private static int numOfMyFriendsImages;				// 好友所有的头像	
+	private static int numOfRightImages;					// 猜对的头像张数
+	private static int numOfWrongImages;					// 猜错的头像张数
 	private static final int RIGHT_FACTOR = 10;				// 答对时相乘的系数
 	private static final int WRONG_FACTOR = 5;				// 答错时相乘的系数
+	private static int difficulty;
 	private static final int BASE_SCORE_EASY = 1000;		// 游戏难度为简单时基础得分 
 	private static final int BASE_SCORE_NORMAL = 1500;		// 游戏难度为一般时基础得分
 	private static final int BASE_SCORE_HARD = 2000;		// 游戏难度为困难时基础得分
-
+	public static final int DIFFICULTY_SIMPLE = 0;
+	public static final int DIFFICULTY_MIDDLE = 1;
+	public static final int DIFFICULTY_HARD = 2;
 
 	/*
 	 * 根据游戏难度进行计分,供主线程调用获取游戏得分
 	 */
-	public static long score(long numOfMyImages1,
-			long numOfMyFriendsImages1, long numOfRightImages1,
-			long numOfWrongImages1, int difficulty) {
+	public static int score(int numOfMyImages1,
+			int numOfMyFriendsImages1, int numOfRightImages1,
+			int numOfWrongImages1, int difficulty1) {
 		
-		numOfMyImages = numOfMyImages1;
-		numOfMyFriendsImages = numOfMyFriendsImages1;
-		numOfRightImages = numOfRightImages1;
-		numOfWrongImages = numOfWrongImages1;
+		init(numOfMyImages1,numOfMyFriendsImages1, 
+				numOfRightImages1,numOfWrongImages1,difficulty1);	
 		
 		switch(difficulty){
-			case 0:
+		
+			case DIFFICULTY_SIMPLE:
 				return easyScore();
-			case 1:
+				
+			case DIFFICULTY_MIDDLE:
 				return normalScore();
-			case 2:
+				
+			case DIFFICULTY_HARD:
 				return hardScore();
+				
 			default:
 				return 0;
 				
@@ -40,10 +45,41 @@ public class GameScorer {
 		
 	}
 	
+	private static void init(int numOfMyImages1,
+			int numOfMyFriendsImages1, int numOfRightImages1,
+			int numOfWrongImages1,int difficulty1) {
+		
+		numOfMyImages = numOfMyImages1;
+		numOfMyFriendsImages = numOfMyFriendsImages1;
+		numOfRightImages = numOfRightImages1;
+		numOfWrongImages = numOfWrongImages1;
+		
+		switch(difficulty1){
+		
+			case 0:
+				difficulty = DIFFICULTY_SIMPLE;
+				break;
+				
+			case 1:
+				difficulty = DIFFICULTY_MIDDLE;
+				break;
+				
+			case 2:
+				difficulty = DIFFICULTY_HARD;
+				break;
+				
+			default:
+				difficulty = DIFFICULTY_SIMPLE;
+				break;
+		}
+		 
+		
+	}
+
 	/*
 	 * 游戏为难度时的计分方式
 	 */
-	private static long easyScore() {
+	private static int easyScore() {
 		return BASE_SCORE_EASY+(numOfRightImages*RIGHT_FACTOR-numOfWrongImages*WRONG_FACTOR)*
 				Math.min(30+30,numOfMyImages+numOfMyFriendsImages);
 	}
@@ -51,7 +87,7 @@ public class GameScorer {
 	/*
 	 * 游戏为一般时的计分方式
 	 */
-	private static long normalScore() {
+	private static int normalScore() {
 		return BASE_SCORE_NORMAL+(numOfRightImages*RIGHT_FACTOR-numOfWrongImages*WRONG_FACTOR)*
 				Math.min(30+30,numOfMyImages+numOfMyFriendsImages);
 	}
@@ -59,7 +95,7 @@ public class GameScorer {
 	/*
 	 * 游戏为困难时的计分方式
 	 */
-	private static long hardScore() {
+	private static int hardScore() {
 		return  BASE_SCORE_HARD+(numOfRightImages*RIGHT_FACTOR-numOfWrongImages*WRONG_FACTOR)*
 				Math.min(30+30,numOfMyImages+numOfMyFriendsImages);
 	}
