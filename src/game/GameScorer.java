51c220adbec6772773c6636440bd1e7d1b4a5b36
lys101250099 <luoyishuai@gmail.com>
2013-06-20 16:07:46 +0800

    修改计分系统,重构部分代码

--- src/game/GameScorer.java
+++ src/game/GameScorer.java
@@ -7,72 +7,37 @@ public class GameScorer {
 	private static long numOfMyFriendsImages;				// 好友所有的头像	
 	private static long numOfRightImages;					// 猜对的头像张数
 	private static long numOfWrongImages;					// 猜错的头像张数
-	private static final int EASY = 0;						// 游戏难度为简单
-	private static final int NORMAL = 1;					// 游戏难度为一般
-	private static final int HARD = 2;						// 游戏难度为困难
-	private static int gameLevel = EASY;					// 游戏的难度级别
 	private static final int RIGHT_FACTOR = 10;				// 答对时相乘的系数
 	private static final int WRONG_FACTOR = 5;				// 答错时相乘的系数
 	private static final int BASE_SCORE_EASY = 1000;		// 游戏难度为简单时基础得分 
 	private static final int BASE_SCORE_NORMAL = 1500;		// 游戏难度为一般时基础得分
 	private static final int BASE_SCORE_HARD = 2000;		// 游戏难度为困难时基础得分
-	private static long myScore;							// 我的得分
+
 
 	/*
-	 * 供主线程调用获取游戏得分
+	 * 根据游戏难度进行计分,供主线程调用获取游戏得分
 	 */
-	
-	public static long getScores(long numOfMyImages,long numOfMyFriendsImages,
-			long numOfRightImages,long numOfWrongImages,int gameLevel){
+	public static long score(long numOfMyImages1,
+			long numOfMyFriendsImages1, long numOfRightImages1,
+			long numOfWrongImages1, int difficulty) {
 		
-		setPara(numOfMyImages,numOfMyFriendsImages,numOfRightImages,numOfWrongImages,gameLevel);
-		return score(gameLevel);
-	}
-	
-	
-	/*
-	 * 设置计算游戏得分的各个参数
-	 */
-	private static void setPara(long numOfMyImages2,
-			long numOfMyFriendsImages2, long numOfRightImages2,
-			long numOfWrongImages2, int gameLevel2) {
+		numOfMyImages = numOfMyImages1;
+		numOfMyFriendsImages = numOfMyFriendsImages1;
+		numOfRightImages = numOfRightImages1;
+		numOfWrongImages = numOfWrongImages1;
 		
-		numOfMyImages = numOfMyImages2;
-		numOfMyFriendsImages = numOfMyFriendsImages2;
-		numOfRightImages = numOfRightImages2;
-		numOfWrongImages = numOfWrongImages2;
-		
-		switch(gameLevel2){
+		switch(difficulty){
 			case 0:
-				gameLevel = EASY;
-				break;
-			case 1:
-				gameLevel = NORMAL;
-				break;
-			case 2:
-				gameLevel = HARD;
-				break;
-				
-		}
-		
-	}
-
-
-	/*
-	 * 根据游戏难度分别计分
-	 */
-	private static long score(int gameLevel){
-	
-		switch(gameLevel){
-			case EASY:
 				return easyScore();
-			case NORMAL:
+			case 1:
 				return normalScore();
-			case HARD:
+			case 2:
 				return hardScore();
 			default:
 				return 0;
+				
 		}
+		
 	}
 	
 	/*
